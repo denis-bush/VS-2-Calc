0edf7f89f3211034a452f8742fb3ce74172a6c2b
denis-bush <denis.bush98@gmail.com>
2022-04-15 16:20:47 +0300

    first commit

---
+++ FormCalc/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FormCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblOper = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnClose.Location = new System.Drawing.Point(240, 180);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(240, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 30);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Сложение";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(240, 145);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 30);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblOper
+            // 
+            this.lblOper.AutoSize = true;
+            this.lblOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblOper.Location = new System.Drawing.Point(125, 10);
+            this.lblOper.Name = "lblOper";
+            this.lblOper.Size = new System.Drawing.Size(0, 16);
+            this.lblOper.TabIndex = 3;
+            this.lblOper.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt1.Location = new System.Drawing.Point(110, 60);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(90, 20);
+            this.txt1.TabIndex = 4;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt2.Location = new System.Drawing.Point(110, 90);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(90, 20);
+            this.txt2.TabIndex = 5;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtResult.Location = new System.Drawing.Point(110, 120);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(90, 20);
+            this.txtResult.TabIndex = 6;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl1.Location = new System.Drawing.Point(10, 60);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(80, 13);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "Первое число:";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl2.Location = new System.Drawing.Point(10, 90);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(78, 13);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "Второе число:";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(10, 120);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(62, 13);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Результат:";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(240, 110);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(100, 30);
+            this.btnDiv.TabIndex = 10;
+            this.btnDiv.Text = "Разделить";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(240, 75);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(100, 30);
+            this.btnMult.TabIndex = 11;
+            this.btnMult.Text = "Умножить";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(240, 40);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(100, 30);
+            this.btnSub.TabIndex = 12;
+            this.btnSub.Text = "Вычитание";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 217);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblOper);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblOper;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnSub;
+    }
+}
+
---
+++ FormCalc/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Закрыть программу
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Очистить все поля
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblOper.Text = "";
+            txt1.Text = "";
+            txt2.Text = "";
+            txtResult.Text = "";
+        }
+
+        // Сумма двух чисел
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double i1, i2, i3;
+            lblOper.Text = "Сложение";
+            i1 = Convert.ToDouble(txt1.Text);
+            i2 = Convert.ToDouble(txt2.Text);
+            i3 = i1 + i2;
+            txtResult.Text = Convert.ToString(i3);
+        }
+
+        // Разность двух чисел
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            double i1, i2, i3;
+            lblOper.Text = "Вычитание";
+            i1 = Convert.ToDouble(txt1.Text);
+            i2 = Convert.ToDouble(txt2.Text);
+            i3 = i1 - i2;
+            txtResult.Text = Convert.ToString(i3);
+        }
+
+        // Произведение двух чисел
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double i1, i2, i3;
+            lblOper.Text = "Умножение";
+            i1 = Convert.ToDouble(txt1.Text);
+            i2 = Convert.ToDouble(txt2.Text);
+            i3 = i1 * i2;
+            txtResult.Text = Convert.ToString(i3);
+        }
+
+        // Частное двух чисел (без деления на 0)
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double i1, i2, i3;
+            lblOper.Text = "Деление";
+            i1 = Convert.ToDouble(txt1.Text);
+            i2 = Convert.ToDouble(txt2.Text);
+            if (i2 == 0)
+            {
+                txtResult.Text = "Ошибка!";
+            }
+            else
+            {
+                i3 = i1 / i2;
+                txtResult.Text = Convert.ToString(i3);
+            }
+        }
+
+    }
+}
